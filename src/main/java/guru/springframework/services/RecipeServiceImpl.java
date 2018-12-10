ed2ecbfd7cef0a849fc184a5122183768ac2a738
Andrii Holub <andr.holub@gmail.com>
2018-12-10 22:57:28 +0200

    bootstrap data

---
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Iterable<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+}
